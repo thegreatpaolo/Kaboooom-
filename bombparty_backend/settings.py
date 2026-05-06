6f23c5e830ba8d001b9e52e97d6f6f9c44352904
keketgwapo <austriakeith08@gmail.com>
2026-05-06 18:01:46 +0800

    fix the frontend to backend connection

--- bombparty_backend/settings.py
+++ bombparty_backend/settings.py
@@ -66,8 +66,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5174',
     'http://localhost:5175',
     'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-    'http://127.0.0.1:5175',
+    'https://kaboooom.vercel.app',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
